63b32a3ebdd2ff52a6303d6e07d92e033699eb7a
Kenny Parnell <k.parnell@gmail.com>
2012-09-21 14:21:35 -0400

    Clean up extconf

--- ext/potracer/extconf.rb
+++ ext/potracer/extconf.rb
@@ -4,13 +4,8 @@ RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
 
 extension_name = 'potracer/potracer'
 
-unless have_header('potracelib.h')
-    raise "potracelib.h not found"
-end
-
-unless have_library('potrace', 'potrace_version')
-    raise "lib potrace not found"
-end
+raise "potracelib.h not found" unless have_header('potracelib.h')
+raise "lib potrace not found" unless have_library('potrace', 'potrace_version')
 
 create_header
 create_makefile(extension_name)
