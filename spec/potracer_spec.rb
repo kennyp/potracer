2d2a4c4b105fcad3b2c5334ac58cec262c242441
Kenny Parnell <k.parnell@gmail.com>
2021-06-29 13:23:43 -0400

    Stop using global must_* and wont_* methods.

--- spec/potracer_spec.rb
+++ spec/potracer_spec.rb
@@ -7,56 +7,56 @@ describe Potracer::Params do
   end
 
   it 'should allow you to get and set the turd size' do
-    @params.turd_size.must_equal(2)
+    _(@params.turd_size).must_equal 2
     @params.turd_size = 3
-    @params.turd_size.must_equal(3)
+    _(@params.turd_size).must_equal 3
   end
 
   it 'should allow you to specify the turnpolicy' do
-    @params.turn_policy.must_equal(Potracer::Turnpolicy::MINORITY)
+    _(@params.turn_policy).must_equal Potracer::Turnpolicy::MINORITY
     @params.turn_policy = Potracer::Turnpolicy::BLACK
-    @params.turn_policy.must_equal(Potracer::Turnpolicy::BLACK)
+    _(@params.turn_policy).must_equal Potracer::Turnpolicy::BLACK
   end
 
   it 'should allow you to specify the alpha_max' do
-    @params.alpha_max.must_equal(1.0)
+    _(@params.alpha_max).must_equal 1.0
     @params.alpha_max = 0
-    @params.alpha_max.must_equal(0.0)
+    _(@params.alpha_max).must_equal 0.0
   end
 
   it 'should allow you to control optomization' do
-    @params.must_be :optimized?
+    _(@params).must_be :optimized?
     @params.unoptimize!
-    @params.wont_be :optimized?
+    _(@params).wont_be :optimized?
     @params.optimize!
-    @params.must_be :optimized?
+    _(@params).must_be :optimized?
   end
 
   it 'should allow you to set the tolerance' do
-    @params.tolerance.must_equal(0.2)
+    _(@params.tolerance).must_equal 0.2
     @params.tolerance = 0
-    @params.tolerance.must_equal(0)
+    _(@params.tolerance).must_equal 0
   end
 end
 
 describe Potracer::Turnpolicy do
   it 'should contain all available turn policies' do
     ps = %w(BLACK WHITE LEFT RIGHT MAJORITY MINORITY RANDOM).map(&:to_sym)
-    ps.all? {|p| Potracer::Turnpolicy.constants.must_include p}
+    ps.all? {|p| _(Potracer::Turnpolicy.constants).must_include p}
   end
 end
 
 describe Potracer::Bitmap do
   it 'should allow you to define a new empty bitmap' do
     bmp = Potracer::Bitmap.new
-    bmp.width.must_equal(250)
-    bmp.height.must_equal(250)
+    _(bmp.width).must_equal 250
+    _(bmp.height).must_equal 250
   end
 
   it 'should let you specify height and width when being created' do
     bmp = Potracer::Bitmap.new(100, 100)
-    bmp.width.must_equal(100)
-    bmp.height.must_equal(100)
+    _(bmp.width).must_equal 100
+    _(bmp.height).must_equal 100
   end
 
   it 'should let you provide the bitmap when being created' do
@@ -68,7 +68,7 @@ describe Potracer::Bitmap do
       [1, 1, 1, 1, 1]
     ]
     bmp = Potracer::Bitmap.new(5, 5, orig)
-    bmp.to_a.must_equal(orig)
+    _(bmp.to_a).must_equal orig
   end
 end
 
@@ -88,7 +88,7 @@ describe Potracer::Trace do
     ])
     params = Potracer::Params.new
     trace = Potracer::Trace.new
-    trace.trace(bmp, params).to_a.wont_be :empty?
+    _(trace.trace(bmp, params).to_a).wont_be :empty?
   end
 
   it 'should allow you to track progress as you trace' do
@@ -96,7 +96,7 @@ describe Potracer::Trace do
     bitmap = Potracer::Bitmap.new
     params = Potracer::Params.new
     Potracer::Trace.new.trace(bitmap, params) { |p| x = p }
-    x.must_equal(100)
+    _(x).must_equal 100
   end
 
   it 'should be able to handle non-homogeneous bitmaps' do
@@ -106,16 +106,16 @@ describe Potracer::Trace do
       [1, 1, 1, 1],
       [1]
     ]
-    Potracer::Trace.bitmap(a).to_a.wont_be_empty
+    _(Potracer::Trace.bitmap(a).to_a).wont_be_empty
   end
 
   it 'should be able to process string representations' do
-    Potracer::Trace.bitmap(image_string, 1886, 601).to_a.wont_be_empty
+    _(Potracer::Trace.bitmap(image_string, 1886, 601).to_a).wont_be_empty
   end
 
   it 'should be able to generate svgs' do
     svg = Potracer::Trace.bitmap(image_string, 1886, 601).to_svg
-    svg.must_match(/^<svg/)
+    _(svg).must_match(/^<svg/)
   end
 
   it 'should be able to use a callback durring tracing' do
@@ -123,6 +123,6 @@ describe Potracer::Trace do
     Potracer::Trace.bitmap(image_string, 1886, 601) do |prog|
       count = prog
     end
-    count.must_equal(100)
+    _(count).must_equal 100
   end
 end
